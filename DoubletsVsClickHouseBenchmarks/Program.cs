13f51b0e61c15e7c5ebe66cc8f829a2ba31d8387
FreePhoenix888 <freephoenix888@gmail.com>
2022-12-31 14:01:17 +0600

    Add benchmark draft

--- DoubletsVsClickHouseBenchmarks/Program.cs
+++ DoubletsVsClickHouseBenchmarks/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using BenchmarkDotNet.Attributes;
+using DoubletsVsClickHouseBenchmarks;
+using TLinkAddress = System.UInt64;
 
-Console.WriteLine(value: "Hello, World!");
+namespace Platform.Data.Doublets.Benchmarks
+{
+    [SimpleJob]
+    [MemoryDiagnoser]
+    public class DoubletsVsClickHouseBenchmarks
+    {
+        public static string csvFilePath = "";
+        public static DateTimeOffset MaximumTime = DateTimeOffset.FromUnixTimeSeconds(DateTimeOffset.Now.ToUnixTimeSeconds());
+        public static DateTimeOffset MinimumTime = DateTimeOffset.FromUnixTimeSeconds(DateTimeOffset.UnixEpoch.ToUnixTimeSeconds());
+        
+        [Benchmark]
+        public void LinksPlatformBenchmark()
+        {
+            Doublets<TLinkAddress> doublets = new Doublets<ulong>();
+            doublets.SaveCandles(csvFilePath);
+            
+        }
+    }
+}
