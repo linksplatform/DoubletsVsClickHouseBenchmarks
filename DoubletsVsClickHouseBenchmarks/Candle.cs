085f7566e5c00964d568685446062789275e0c71
FreePhoenix888 <freephoenix888@gmail.com>
2022-12-31 14:01:41 +0600

    Add Candle struct

---
+++ DoubletsVsClickHouseBenchmarks/Candle.cs
@@ -0,0 +1,11 @@
+namespace DoubletsVsClickHouseBenchmarks;
+
+public struct Candle
+{
+    public DateTimeOffset StartingTime;
+    public decimal OpeningPrice;
+    public decimal ClosingPrice;
+    public decimal HighestPrice;
+    public decimal LowestPrice;
+    public long Volume;
+}
