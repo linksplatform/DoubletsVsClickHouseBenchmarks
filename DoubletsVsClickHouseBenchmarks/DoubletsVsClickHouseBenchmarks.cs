6d26764852ca21b64fbc03aadf4325b4344e0452
FreePhoenix888 <freephoenix888@gmail.com>
2023-03-01 15:15:45 +0600

    Use CsvCandleParser

--- DoubletsVsClickHouseBenchmarks/DoubletsVsClickHouseBenchmarks.cs
+++ DoubletsVsClickHouseBenchmarks/DoubletsVsClickHouseBenchmarks.cs
@@ -30,29 +30,7 @@ public class DoubletsVsClickHouseBenchmarks
     [GlobalSetup]
     public void GlobalSetup()
     {
-        List<Candle> candles = new List<Candle>();
-        using var reader = new StreamReader(path: CsvFilePath);  
-        CsvConfiguration config = new CsvConfiguration(CultureInfo.InvariantCulture)
-        {
-            NewLine = Environment.NewLine,
-            Delimiter = ";",
-        };
-        using var csv = new CsvReader(reader, config);
-        csv.Read();
-        while (csv.Read())
-        {
-            var candle = new Candle
-            {
-                StartingTime = DateTimeOffset.FromUnixTimeMilliseconds(csv.GetField<long>(0)),
-                OpeningPrice = csv.GetField<decimal>(1),
-                ClosingPrice = csv.GetField<decimal>(2),
-                HighestPrice = csv.GetField<decimal>(3),
-                LowestPrice = csv.GetField<decimal>(4),
-                Volume = csv.GetField<long>(5)
-            };
-            candles.Add(candle);
-        }
-        Candles = candles;
+        Candles = new CsvCandleParser().Parse(CsvFilePath).ToList();
     }
     
     [Benchmark]
